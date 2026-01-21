09461db861dc71d7d401abddc06d5c85d39b8c7a
celinasakar3-ops <celinasarkar3@gmail.com>
2026-01-21 13:51:29 +0530

    updated salary calculator with f-string and conditoins

--- salary_calculator.py
+++ salary_calculator.py
@@ -1,14 +1,29 @@
-Employee = str(input("Enter Employee Name: "))
-Salary = float(input("Enter your Salary: "))
-Bonus = int(input("Enter your bonus Amount: "))
-Tax_rate = float(input("Enter your Tax rate: "))
+employee = []
+while True:
+    Employee = str(input("Enter Employee Name: "))
+    Salary = float(input("Enter your Salary: "))
+    Bonus = int(input("Enter your bonus Amount: "))
+    Tax_rate = float(input("Enter your Tax rate: "))
+
+    if Salary < 0 or Bonus < 0 :
+        print("Invalid Input! Salary and Bonus must be non-negative.")
+        continue
 
 
-Gross_salary = Salary + Bonus
-Tax_Amount = (Gross_salary*Tax_rate)/100
-Net_Salary = Gross_salary - Tax_Amount 
+    Gross_salary = Salary + Bonus
+    Tax_Amount = (Gross_salary*Tax_rate)/100
+    Net_Salary = Gross_salary - Tax_Amount 
 
-print("------------------------")
-print(Employee,"your Gross Salary is: ", Gross_salary)
-print(Employee,"your Tax Amount is: ", Tax_Amount)
-print(Employee,"your Net Salary is: ", Net_Salary)
+    employee.append([Employee, Salary, Bonus, Tax_rate, Gross_salary, Tax_Amount, Net_Salary])
+    
+    print("------------------------")
+    print(f'{Employee} Salary Details:')
+    print(f"Gross Salary: {Gross_salary}")
+    print(f"Tax Amount: {Tax_Amount}")
+
+
+
+    choice = input("Do you want to add another employee? (yes/no): ").strip().lower()
+    if choice != 'yes':
+        break
+print("\nSummary of all employees:" , employee)
